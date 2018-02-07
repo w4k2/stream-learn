d564007177207733c6f4a0e381261b1c99b42d1b
Pawel Ksieniewicz <pawel@ksienie.com>
2018-02-07 15:34:25 +0100

    pip

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -21,48 +21,15 @@ export CXX=/usr/lib/ccache/g++
 # ~60M is used by .ccache when compiling from scratch at the time of writing
 ccache --max-size 100M --show-stats
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
-
-    # Install miniconda
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    MINICONDA_PATH=/home/travis/miniconda
-    chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
-    export PATH=$MINICONDA_PATH/bin:$PATH
-    conda update --yes conda
-
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION pip
-    source activate testenv
-    conda install --yes numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION pandas
-
-    if [[ "$SKLEARN_VERSION" == "master" ]]; then
-        conda install --yes cython
-        pip install -U git+https://github.com/scikit-learn/scikit-learn.git
-    else
-        conda install --yes scikit-learn=$SKLEARN_VERSION
-    fi
-
-    conda install --yes nose pytest pytest-cov
-    # Install nose-timer via pip
-    pip install nose-timer codecov enum liac-arff
-
-elif [[ "$DISTRIB" == "ubuntu" ]]; then
-    # At the time of writing numpy 1.9.1 is included in the travis
-    # virtualenv but we want to use the numpy installed through apt-get
-    # install.
-    deactivate
-    # Create a new virtualenv using system site packages for python, numpy
-    virtualenv --system-site-packages testvenv
-    source testvenv/bin/activate
-    pip install scikit-learn pandas nose nose-timer pytest pytest-cov codecov \
-        sphinx numpydoc
-
-fi
+# At the time of writing numpy 1.9.1 is included in the travis
+# virtualenv but we want to use the numpy installed through apt-get
+# install.
+deactivate
+# Create a new virtualenv using system site packages for python, numpy
+virtualenv --system-site-packages testvenv
+source testvenv/bin/activate
+pip install scikit-learn pandas nose nose-timer pytest pytest-cov codecov \
+    sphinx numpydoc enum liac-arff
 
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
@@ -70,5 +37,3 @@ python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 python setup.py develop
 ccache --show-stats
-# Useful for debugging how ccache is used
-# cat $CCACHE_LOGFILE
