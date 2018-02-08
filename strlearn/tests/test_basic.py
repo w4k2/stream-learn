4cebda5b86dced52bb22f6f5dc121c9b189677dc
Pawel Ksieniewicz <pawel@ksienie.com>
2018-02-08 13:23:37 +0100

    Toyset.

--- strlearn/tests/test_basic.py
+++ strlearn/tests/test_basic.py
@@ -1,13 +1,17 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from sklearn import tree
-from strlearn import WAE
-from strlearn import streamLearner
+import WAE
+import streamLearner
+
+dbname = 'toyset'
 
 def test_something():
     assert 5 == 5
 
 def test_WAE():
     base_clf = tree.DecisionTreeClassifier()
-    dbname = 'RBFGradualRecurring'
     clf = WAE.WAE(base_classifier = base_clf)
     stream = open('datasets/%s.arff' % dbname, 'r')
     learner = streamLearner.StreamLearner(stream, clf, verbose=False)
