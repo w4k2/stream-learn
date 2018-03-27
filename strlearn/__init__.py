4d99cb6d48715a576d6a1d802fc06f787874701e
JakubKlik <abuk9999@gmail.com>
2018-03-27 17:36:54 +0200

    Import fix

--- strlearn/__init__.py
+++ strlearn/__init__.py
@@ -1,10 +1,10 @@
 # Empty
 from ._version import __version__
 
+import utils
 from learner import Learner
 import controllers
 import ensembles
-import utils
 
 __all__ = [
     'ensembles', 'controllers', '__version__', 'utils'
