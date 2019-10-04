bbd036117aa0e142faf9fa3ef23778e4feb8b90f
Pawel Ksieniewicz <pawel@ksienie.com>
2019-10-04 14:23:09 +0200

    CBE sklearn test.

--- strlearn/ensembles/ChunkBasedEnsemble.py
+++ strlearn/ensembles/ChunkBasedEnsemble.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_X_y, check_array
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import _check_partial_fit_first_call
 from sklearn import base
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
@@ -8,31 +9,46 @@ from sklearn.naive_bayes import GaussianNB
 class ChunkBasedEnsemble(BaseEstimator, ClassifierMixin):
     def __init__(self, ensemble_size=5):
         self.ensemble_size = ensemble_size
-        self.ensemble_ = []
+        # self.ensemble_ = []
 
-    def set_base_clf(self, base_clf=GaussianNB()):
+    def set_base_clf(self, base_clf=GaussianNB):
         self._base_clf = base_clf
 
     def fit(self, X, y):
-        self.set_base_clf()
+        self.partial_fit(X, y)
+
+        return self
 
     def partial_fit(self, X, y, classes=None):
+        X, y = check_X_y(X, y)
         if not hasattr(self, "_base_clf"):
             self.set_base_clf()
-        X, y = check_X_y(X, y)
+            self.ensemble_ = []
 
+        # Check feature consistency
+        if hasattr(self, "X_"):
+            if self.X_.shape[1] != X.shape[1]:
+                raise ValueError("number of features does not match")
         self.X_, self.y_ = X, y
 
-        self.ensemble_.append(base.clone(self._base_clf).fit(self.X_, self.y_))
+        # Check classes
+        self.classes_ = classes
+        if self.classes_ is None:
+            self.classes_, _ = np.unique(y, return_inverse=True)
+
+        self.ensemble_.append(self._base_clf().fit(self.X_, self.y_))
 
         if len(self.ensemble_) > self.ensemble_size:
             del self.ensemble_[0]
+
+        return self
 
     def ensemble_support_matrix(self, X):
         return np.array([member_clf.predict_proba(X) for member_clf in self.ensemble_])
 
     def predict(self, X):
-
+        # Check is fit had been called
+        check_is_fitted(self, "classes_")
         X = check_array(X)
         if X.shape[1] != self.X_.shape[1]:
             raise ValueError("number of features does not match")
@@ -41,4 +57,5 @@ class ChunkBasedEnsemble(BaseEstimator, ClassifierMixin):
         average_support = np.mean(esm, axis=0)
         prediction = np.argmax(average_support, axis=1)
 
-        return prediction
+        return self.classes_[prediction]
+        # return prediction
---
+++ strlearn/tests/test_sklearn.py
@@ -0,0 +1,11 @@
+"""Basic tests."""
+import sys
+
+import strlearn as sl
+from sklearn.utils.estimator_checks import check_estimator
+
+sys.path.insert(0, "../..")
+
+
+def test_ChunkBasedEnsemble():
+    check_estimator(sl.ensembles.ChunkBasedEnsemble)
