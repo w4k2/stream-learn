6ae4a9e992c9a4f2046105d844c7c9a6c74f3ddb
Pawel Ksieniewicz <pawel@ksienie.com>
2019-10-04 14:29:55 +0200

    Drying tests.

--- strlearn/tests/test_generators.py
+++ strlearn/tests/test_generators.py
@@ -7,14 +7,24 @@ sys.path.insert(0, "../..")
 from sklearn.neural_network import MLPClassifier
 
 
+def test_generators_drying():
+    stream = sl.generators.DriftedStream()
+    while stream.get_chunk():
+        pass
+    stream = sl.generators.StationaryStream()
+    while stream.get_chunk():
+        pass
+
+
 def test_generator_drifted():
-    stream = sl.generators.DriftedStream(sigmoid_spacing=999)
-    clf = MLPClassifier(hidden_layer_sizes=(100,))
+    stream = sl.generators.DriftedStream()
+    clf = MLPClassifier()
     evaluator = sl.evaluators.TestThenTrainEvaluator()
     evaluator.process(clf, stream)
 
+
 def test_generator_stationary():
     stream = sl.generators.StationaryStream()
-    clf = MLPClassifier(hidden_layer_sizes=(100,))
+    clf = MLPClassifier()
     evaluator = sl.evaluators.TestThenTrainEvaluator()
     evaluator.process(clf, stream)
