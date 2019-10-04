b1c0d74930d5b8e03bdcdcec6943bf972b24f400
Pawel Ksieniewicz <pawel@ksienie.com>
2019-10-04 15:41:32 +0200

    Illustrative script.

--- examples/gradual_drift.py
+++ examples/gradual_drift.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ==========================
-Gradual drift
+Incremental drift
 ==========================
 This example shows a basic stream processing using WAE algorithm.
 
@@ -18,21 +18,21 @@ import numpy as np
 
 stream = DriftedStream(
     n_chunks=100,
-    chunk_size=200,
-    random_state=5,
+    chunk_size=100,
+    random_state=105,
     n_features=2,
     n_classes=2,
     n_drifts=2,
     n_informative=2,
     n_redundant=0,
     n_repeated=0,
-    sigmoid_spacing=4,
+    sigmoid_spacing=5,
     n_clusters_per_class=1,
 )
 
-checkpoints = np.linspace(0, stream.n_chunks - 1, 11).astype(int)
+checkpoints = np.linspace(0, stream.n_chunks - 1, 8).astype(int)
 
-fig = plt.figure(constrained_layout=True, figsize=(11, 4))
+fig = plt.figure(constrained_layout=True, figsize=(8, 4))
 
 gs = GridSpec(3, len(checkpoints), figure=fig)
 
@@ -48,25 +48,25 @@ for i in range(100):
 
     if i in checkpoints:
         index = np.where(checkpoints == i)[0][0]
-        ax = fig.add_subplot(gs[0, index])
+        ax = fig.add_subplot(gs[2, index])
         ax.scatter(X[:, 0], X[:, 1], c=y, s=10, alpha=0.5, cmap="bwr")
-        ax.set_title("c%i" % i, fontsize=8)
         ax.set_xlim(-5, 5)
         ax.set_ylim(-5, 5)
         ax.set_xticks([])
         ax.set_yticks([])
 
-# Periodical sigmoid
-ax = fig.add_subplot(gs[2, :])
-ax.set_title("Periodical sigmoid (ss=%.1f)" % stream.sigmoid_spacing)
-ax.plot(stream.period_sigmoid, lw=1, c="black")
-
 # Concept presence
 ax = fig.add_subplot(gs[1, :])
 ax.set_title("Concept presence")
-ax.plot(a, c="green")
-ax.plot(b, c="purple")
+ax.plot(a, c="black", ls=":")
+ax.plot(b, c="black", ls="--")
 ax.set_ylim(-10, stream.chunk_size + 10)
+ax.set_xticks(checkpoints)
+
+# Periodical sigmoid
+ax = fig.add_subplot(gs[0, :])
+ax.set_title("Periodical sigmoid (ss=%.1f)" % stream.sigmoid_spacing)
+ax.plot(stream.period_sigmoid, lw=1, c="black")
+
 
 plt.savefig("gradual_drift.png")
-plt.show()
