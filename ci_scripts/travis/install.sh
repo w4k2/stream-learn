8bbf78063818831b98e9d673a95ae5d17ea630ab
Pawel Ksieniewicz <pawel@ksienie.com>
2019-10-05 18:47:07 +0200

    Clean-up.

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -1,6 +1,2 @@
 pip install pytest pytest-cov codecov matplotlib coverage coveralls
-
-python --version
-python -c "import numpy; print('numpy %s' % numpy.__version__)"
-python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python setup.py develop
