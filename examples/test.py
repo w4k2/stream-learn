a1030d85123c93b192fd9420ba98b1be303e94f3
Pawel Ksieniewicz <pawel@ksienie.com>
2019-11-21 15:51:58 +0100

    Example docset.

--- examples/test.py
+++ examples/test.py
@@ -1,17 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+=======
+Example
+=======
+Just example.
+
+"""
 import strlearn as sl
 from sklearn.naive_bayes import GaussianNB
 from sklearn.base import clone
 import numpy as np
-
-# stream = sl.streams.StreamGenerator(n_chunks=5, chunk_size=500, weights=[0.2, 0.8], n_drifts=3, concept_sigmoid_spacing=999)
-# clf1 = sl.ensembles.OOB(time_decay_factor=0.9)
-# clf2 = sl.ensembles.UOB(time_decay_factor=0.9)
-# clf3 = sl.ensembles.OnlineBagging()
-# clf.set_base_clf(sl.ensembles.SampleWeightedMetaEstimator)
-##clf4 = sl.ensembles.ChunkBasedEnsemble()
-# clf5 = sl.classifiers.AccumulatedSamplesClassifier()
-
-# clf = {clf1, clf2, clf3, clf4, clf5}
 
 clf = [GaussianNB(), GaussianNB()]
 
