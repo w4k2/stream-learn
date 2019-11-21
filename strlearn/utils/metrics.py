526bc88488a1c4c8e3dff6dfc510e840f2234d4c
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2019-11-21 10:07:26 +0100

    gmean.

--- strlearn/utils/metrics.py
+++ strlearn/utils/metrics.py
@@ -137,12 +137,18 @@ def geometric_mean_score(y_true, y_pred):
     gmean : float
     """
 
-    neg_true = 1 - y_true
-    neg_pred = 1 - y_pred
+    P = y_true == 1
+    N = y_true == 0
 
-    recall_1 = recall(neg_true[neg_true == 1], neg_pred[neg_true == 1])
-    recall_2 = recall(y_true[y_true == 1], y_pred[y_true == 1])
+    TP = np.sum(y_pred[P] == 1)
+    FP = np.sum(y_pred[P] == 0)
 
-    gmean = (recall_1 * recall_2)**(1/2)
+    TN = np.sum(y_pred[N] == 0)
+    FN = np.sum(y_pred[N] == 1)
+
+    recall_a = TP / (TP + FP)
+    recall_b = TN / (TN + FN)
+
+    gmean = (recall_a * recall_b)**(1/2)
 
     return np.nan_to_num(gmean)
