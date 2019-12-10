a50ef87630e03425a0d9651415c9f358312f5c28
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2019-12-10 14:47:03 +0100

    Last time.

--- doc/conf.py
+++ doc/conf.py
@@ -30,8 +30,7 @@ extensions = [
     "sphinx.ext.mathjax",
 ]
 
-# mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
-mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+mathjax_path="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?..."
 
 # this is needed for some reason...
 # see https://github.com/numpy/numpydoc/issues/69
