0e4a9d70016d8e8b62afa76d401b1bdfd45885ce
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2020-04-27 16:08:23 +0200

    Typo.

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -1,2 +1,2 @@
-pip install pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm, numpy
+pip install pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm numpy
 python setup.py develop
