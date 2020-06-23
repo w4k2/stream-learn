431e244cba164d17d277a727ae8356278f5d54a8
JakubKlik <jakub.klikowski@pwr.edu.pl>
2020-06-23 16:02:16 +0200

    Fix.

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -1,2 +1,2 @@
-pip install -upgrade pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm numpy imbalanced-learn
+pip install --upgrade pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm numpy imbalanced-learn
 python setup.py develop
