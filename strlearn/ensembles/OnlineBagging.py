29dd9d99cc58eef1c1d73fa367e96e6f16ef08a3
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2021-11-12 15:27:00 +0100

    OnlineBagging integration.

--- strlearn/ensembles/OnlineBagging.py
+++ strlearn/ensembles/OnlineBagging.py
@@ -1,45 +1,26 @@
-"""Online Bagging."""
-
 import numpy as np
-from sklearn.base import ClassifierMixin, clone
-from sklearn.ensemble import BaseEnsemble
-from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
+from sklearn.base import clone
+from ..ensembles.base import StreamingEnsemble
 
-
-class OnlineBagging(BaseEnsemble, ClassifierMixin):
+class OnlineBagging(StreamingEnsemble):
     """
     Online Bagging.
     """
 
     def __init__(self, base_estimator=None, n_estimators=10):
         """Initialization."""
-        self.base_estimator = base_estimator
-        self.n_estimators = n_estimators
-
-    def fit(self, X, y):
-        """Fitting."""
-        self.partial_fit(X, y)
-        return self
+        super().__init__(base_estimator, n_estimators)
 
     def partial_fit(self, X, y, classes=None):
         """Partial fitting."""
-        X, y = check_X_y(X, y)
+        super().partial_fit(X, y, classes)
+        if not self.green_light:
+            return self
 
-        if not hasattr(self, "ensemble_"):
+        if len(self.ensemble_) == 0:
             self.ensemble_ = [
                 clone(self.base_estimator) for i in range(self.n_estimators)
             ]
-
-        # Check feature consistency
-        if hasattr(self, "X_"):
-            if self.X_.shape[1] != X.shape[1]:
-                raise ValueError("number of features does not match")
-        self.X_, self.y_ = X, y
-
-        # Check classes
-        self.classes_ = classes
-        if self.classes_ is None:
-            self.classes_, _ = np.unique(y, return_inverse=True)
 
         self.weights = []
         for instance in range(self.X_.shape[0]):
@@ -56,31 +37,3 @@ class OnlineBagging(BaseEnsemble, ClassifierMixin):
             )
 
         return self
-
-    def ensemble_support_matrix(self, X):
-        """Ensemble support matrix."""
-        return np.array([member_clf.predict_proba(X) for member_clf in self.ensemble_])
-
-    def predict(self, X):
-        """
-        Predict classes for X.
-
-        :type X: array-like, shape (n_samples, n_features)
-        :param X: The training input samples.
-
-        :rtype: array-like, shape (n_samples, )
-        :returns: The predicted classes.
-        """
-
-        # Check is fit had been called
-        check_is_fitted(self, "classes_")
-        X = check_array(X)
-        if X.shape[1] != self.X_.shape[1]:
-            raise ValueError("number of features does not match")
-
-        esm = self.ensemble_support_matrix(X)
-        average_support = np.mean(esm, axis=0)
-        prediction = np.argmax(average_support, axis=1)
-
-        # Return prediction
-        return self.classes_[prediction]
